6e5ff3b10372d9fdad35d08c8527ed75a57a4f82
heesunc <112846188+heesunc@users.noreply.github.com>
2024-11-14 12:29:18 +0900

    Title: 두 배열의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/두 배열의 곱/multiple-of-two-arrays.cpp
+++ 241114/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -2,38 +2,30 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-
     int arr[3][3];
     int arr2[3][3];
 
-    for(int i=0; i<3; i++)
-    {
-        for(int j=0; j<3; j++)
-        {
+    // 첫 번째 배열 입력
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
             cin >> arr[i][j];
         }
     }
 
-    int num = 2;
-    for(int i=0; i<3; i++)
-    {
-        for(int j=0; j<3; j++)
-        {
-            arr2[i][j] = num;
-            num += 1;
+    // 두 번째 배열 입력
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> arr2[i][j];
         }
     }
 
-    int result[3][3];
-    for(int i=0; i<3; i++)
-    {
-        for(int j=0; j<3; j++)
-        {
-            result[i][j] = arr[i][j] * arr2[i][j];
-            cout << result[i][j] << " ";
+    // 두 배열의 같은 위치 요소끼리 곱하고 출력
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << arr[i][j] * arr2[i][j] << " ";
         }
         cout << "\n";
     }
+
     return 0;
 }
