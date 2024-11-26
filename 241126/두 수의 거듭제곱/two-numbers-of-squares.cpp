226197b61a46571f8bee11535880defed963e2da
heesunc <112846188+heesunc@users.noreply.github.com>
2024-11-26 13:13:30 +0900

    Title: 두 수의 거듭제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241126/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>  // pow 함수 사용을 위한 헤더 파일
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b; cin >> a>>b;
+    int result = pow(a,b);
+    cout << result;
+    return 0;
+}
