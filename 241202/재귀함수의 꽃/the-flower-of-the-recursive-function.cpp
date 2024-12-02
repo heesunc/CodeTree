7d7a1ae5a11d2983bc2e09fd78df52966b6cd76d
heesunc <112846188+heesunc@users.noreply.github.com>
2024-12-02 11:02:26 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241202/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// 1부터 N까지 출력하는 재귀 함수
+void printAscending(int n) {
+    if (n == 0) return; // 종료 조건
+    printAscending(n - 1); // n-1까지 재귀 호출
+    cout << n << " "; // 현재 숫자 출력
+}
+
+// N부터 1까지 출력하는 재귀 함수
+void printDescending(int n) {
+    if (n == 0) return; // 종료 조건
+    cout << n << " "; // 현재 숫자 출력
+    printDescending(n - 1); // n-1까지 재귀 호출
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    printDescending(n);
+    printAscending(n);
+
+    return 0;
+}
