fac6ea7e2c47e354e098a4f27f569523ec0e3960
heesunc <112846188+heesunc@users.noreply.github.com>
2024-12-09 10:47:33 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241209/단어 정렬/sorting-words.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<string> arr(n);
+    
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
